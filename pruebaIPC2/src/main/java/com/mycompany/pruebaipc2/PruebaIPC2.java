0e45fd1999d3774ad39f59aa26d48a1920cbba24
chedat0 <jeffersongomez201830892@cunoc.edu.gt>
2026-02-03 20:02:08 -0600

    Subiendo proyecto de Java

---
+++ pruebaIPC2/src/main/java/com/mycompany/pruebaipc2/PruebaIPC2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebaipc2;
+
+/**
+ *
+ * @author jeffm
+ */
+public class PruebaIPC2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Prueba de Git y Github");
+    }
+}
